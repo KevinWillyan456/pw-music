88bf02b42bca179b151f930c3cc1d57db5184ffb
Kevin Souza <kevinwillyansouza@gmail.com>
2023-07-10 19:29:55 -0400

    feat: CRUD do user-favorites criado

---
+++ src/app/api/user/user-favorites/[userFavoriteId]/route.ts
@@ -0,0 +1,61 @@
+import { prisma } from '@/lib/prisma'
+import { NextResponse } from 'next/server'
+
+export async function PUT(
+  request: Request,
+  { params: { userFavoriteId } }: { params: { userFavoriteId: number } }
+) {
+  const req = await request.json()
+
+  const userIdFormated = Number(userFavoriteId)
+
+  const { musicId, userId } = req
+
+  if (!musicId && !userId) {
+    return new NextResponse(
+      JSON.stringify({
+        error: {
+          code: 'MISSING_DATA',
+        },
+      }),
+      { status: 400 }
+    )
+  }
+
+  await prisma.userFavorites.update({
+    where: {
+      id: userIdFormated,
+    },
+    data: {
+      musicId,
+      userId,
+    },
+  })
+
+  return new NextResponse(
+    JSON.stringify({
+      success: true,
+    }),
+    { status: 200 }
+  )
+}
+
+export async function DELETE(
+  request: Request,
+  { params: { userFavoriteId } }: { params: { userFavoriteId: number } }
+) {
+  const userIdFormated = Number(userFavoriteId)
+
+  await prisma.userFavorites.delete({
+    where: {
+      id: userIdFormated,
+    },
+  })
+
+  return new NextResponse(
+    JSON.stringify({
+      success: true,
+    }),
+    { status: 200 }
+  )
+}
